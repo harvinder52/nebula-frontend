2cb7c0ed9ede4882ae4e084ff88e7aba363f8abc
Harv <85876365+harvinder52@users.noreply.github.com>
2023-09-10 16:05:59 +0530

    fixed routing

--- components/Cards.jsx
+++ components/Cards.jsx
@@ -24,7 +24,7 @@ function Cards() {
             <a
               href="#"
               className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              onClick={() => router.push("/form")}
+              onClick={() => router.push("/Form")}
             >
               Apply Now!
               <svg
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,23 @@
 "use client";
 import "tailwindcss/tailwind.css";
 import React from "react";
+import Link from "next/link";
 import Home from "../components/Home.jsx";
 
 import Form from "./Form";
 
 function App() {
   return (
-    <div>
-      <Home></Home>
-    </div>
+    <ul>
+      <li>
+        <Link href="/">
+          <Home></Home>
+        </Link>
+      </li>
+      <li>
+        <Link href="/Form"></Link>
+      </li>
+    </ul>
   );
 }
 export default App;
